754cae9062cccbdd1de4b40626273b90e0eaa8f4
rogorman9 <rogorman9@gmail.com>
2016-12-08 01:06:17 -0500

    Can query oracle

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ app.use(express.static(__dirname + '/'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-
-
-
-
 function doRelease(connection)
 {
   connection.release(
@@ -22,12 +18,12 @@ function doRelease(connection)
 }
 
 app.get('/', function (req, res) {
-	res.sendfile('./views/index.html');
+	res.sendFile('./views/index.html');
 });
 
 // When the server receives a get request with the path /hello, it will respnd by send an html file back to the client using the sendfile() function
 app.get('/hello', function(req, res){
-	res.sendfile("./hello.html");
+	res.sendFile("./hello.html");
 });
 
 app.get('/search', function (req, res) {
@@ -37,9 +33,9 @@ app.get('/search', function (req, res) {
 app.get('/test', function (req, res) {
 	oracledb.getConnection(
 	  {
-	    user          : "cis550projectlkr",
-	    password      : "cis550projectcgo",
-	    connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=project.c2ntpcxpaf12.us-west-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SID=mydb)))"
+	    user          : "cis550projectklr",
+	    password      : "cis550projectgco",
+	    connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=cis550project.czyk6pisuibz.us-west-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SID=MyDB)))"
 	  },
 	  function(err, connection)
 	  {
@@ -48,7 +44,7 @@ app.get('/test', function (req, res) {
 	      return;
 	    }
 	    connection.execute(
-	      "SELECT * FROM MEDAL m INNER JOIN ATHLETE a ON m.athlete_id = a.id WHERE a.name LIKE \'" + req.query.query + "%\';",
+	      "SELECT * FROM MEDAL m INNER JOIN ATHLETE a ON m.athlete_id = a.id WHERE a.name LIKE \'" + req.query.query + "%\'",
 	      function(err, result)
 	      {
 	        if (err) {
